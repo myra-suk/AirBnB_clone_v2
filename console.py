18fd3483be8b2e0773fb6a9bbc982d7775f40326
myra-suk <112114209+myra-suk@users.noreply.github.com>
2023-03-22 18:30:33 +0300

    Update console.py

--- console.py
+++ console.py
@@ -37,7 +37,6 @@ class HBNBCommand(cmd.Cmd):
 
     def precmd(self, line):
         """Reformat command line for advanced command syntax.
-
         Usage: <class name>.<command>([<id> [<*args> or <**kwargs>]])
         (Brackets denote optional fields in usage example.)
         """
@@ -114,7 +113,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """ Create an object of any class"""
+        """ Create an object of given parameters"""
         if not args:
             print("** class name missing **")
             return
